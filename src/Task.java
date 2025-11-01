5ca8140017d34962cff0256beddab945c7ebf6af
MikeBarney88 <michaelbarney1988@gmail.com>
2025-11-01 15:22:19 -0230

    Commit 1 - Created code for Task.java.

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
 
 public class Task {
+    private String description;
+    private boolean isCompleted;
+
+    public Task (String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public void markAsCompleted() {
+        this.isCompleted = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    @Override
+    public String toString() {
+       String status = isCompleted ? "[X]" : "[ ]";
+       return status + " " + description;
+    }
 }
